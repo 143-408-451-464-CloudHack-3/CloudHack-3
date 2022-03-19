2fd8de95f4efdc4fe720664beb5536bbab73c3ff
arjunvuppala123 <arjunv2001@gmail.com>
2022-03-19 11:58:59 +0530

    added extra features

--- microservices/landing/app/app.py
+++ microservices/landing/app/app.py
@@ -19,6 +19,47 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1/n2
 
+def gcd(n1, n2):
+    while(n2):
+       n1, n2 = n2, n1 % n2
+    return n1
+
+def lcm(n1, n2):
+    if n1 > n2:
+       greater = n1
+    else:
+       greater = n2
+    while(True):
+       if((greater % n1 == 0) and (greater % n2 == 0)):
+           lcm = greater
+           break
+       greater += 1
+    return lcm
+
+def modulus(n1, n2):
+    return n1%n2
+
+def exponent(n1, n2):
+    return n1**n2
+
+def greater(n1, n2):
+    if n1 > n2:
+        return True
+    else:
+        return False
+
+def less(n1, n2):
+    if n1 < n2:
+        return True
+    else:
+        return False
+
+def equal(n1, n2):
+    if n1 == n2:
+        return True
+    else:
+        return False
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
     number_1 = request.form.get("first")
@@ -39,6 +80,20 @@ def index():
         result = multiply(number_1, number_2)
     elif operation == 'divide':
         result = divide(number_1, number_2)
+    elif operation == 'GCD':
+        result = gcd(number_1, number_2)
+    elif operation == 'LCM':
+        result = lcm(number_1, number_2)
+    elif operation == 'modulus':
+        result = modulus(number_1)
+    elif operation == 'exponent':
+        result = exponent(number_1, number_2)
+    elif operation == 'greater':
+        result = greater(number_1, number_2)
+    elif operation == 'less':
+        result = less(number_1, number_2)
+    elif operation == 'equal':
+        result = equal(number_1, number_2)
 
     flash(f'The result of operation {operation} on {number_1} and {number_2} is {result}')
 
