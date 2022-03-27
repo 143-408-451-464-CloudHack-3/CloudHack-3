280287eb2a29d8f6178342802166458d3fd38ced
arjunvuppala123 <arjunv2001@gmail.com>
2022-03-27 18:18:26 +0530

    final commit

--- microservices/landing/app/app.py
+++ microservices/landing/app/app.py
@@ -14,7 +14,7 @@ def index():
     if num1 == None:
         num1 = '0'
     if num2 == None:
-        num2 = '0'
+        num2 = '0'             
     operation = request.form.get('operation')
     result = 0
     if operation == 'add':
@@ -25,6 +25,20 @@ def index():
         result = requests.get('http://multiplication:5047/mul/'+str(num1) +'/'+str(num2)).text
     elif operation == 'divide':
         result = requests.get('http://division:5049/div/'+str(num1) +'/'+str(num2)).text
+    elif operation == 'GCD':
+        result = requests.get('http://gcd:5053/gcd/'+str(num1) +'/'+str(num2)).text
+    elif operation == 'LCM':
+        result = requests.get('http://lcm:5055/lcm/'+str(num1) +'/'+str(num2)).text
+    elif operation == 'modulus':
+        result = requests.get('http://modulus:5057/modulus/'+str(num1) +'/'+str(num2)).text
+    elif operation == 'exponent':
+        result = requests.get('http://exponent:5052/exponent/'+str(num1) +'/'+str(num2)).text
+    elif operation == 'greater':
+        result = requests.get('http://greatthan:5054/greatthan/'+str(num1) +'/'+str(num2)).text
+    elif operation == 'less':
+        result = requests.get('http://lessthan:5056/lessthan/'+str(num1) +'/'+str(num2)).text
+    elif operation == 'equal':
+        result = requests.get('http://equalto:5051/equalto/'+str(num1) +'/'+str(num2)).text
 
     flash(f'The result of operation {operation} on {num1} and {num2} is {result}')
 
@@ -36,77 +50,3 @@ if __name__ == '__main__':
         port=5050,
         host="0.0.0.0"
     )
-
-
-'''
-def add(n1, n2):
-    return n1+n2
-
-def minus(n1, n2):
-    return n1-n2
-
-def multiply(n1, n2):
-    return n1*n2
-
-def divide(n1, n2):
-    return n1/n2
-
-
-def gcd(n1, n2):
-    while(n2):
-       n1, n2 = n2, n1 % n2
-    return n1
-
-def lcm(n1, n2):
-    if n1 > n2:
-       greater = n1
-    else:
-       greater = n2
-    while(True):
-       if((greater % n1 == 0) and (greater % n2 == 0)):
-           lcm = greater
-           break
-       greater += 1
-    return lcm
-
-def modulus(n1, n2):
-    return n1%n2
-
-def exponent(n1, n2):
-    return n1**n2
-
-def greater(n1, n2):
-    if n1 > n2:
-        return True
-    else:
-        return False
-
-def less(n1, n2):
-    if n1 < n2:
-        return True
-    else:
-        return False
-
-def equal(n1, n2):
-    if n1 == n2:
-        return True
-    else:
-        return False
-'''
-
-'''
-    elif operation == 'GCD':
-        result = gcd(number_1, number_2)
-    elif operation == 'LCM':
-        result = lcm(number_1, number_2)
-    elif operation == 'modulus':
-        result = modulus(number_1)
-    elif operation == 'exponent':
-        result = exponent(number_1, number_2)
-    elif operation == 'greater':
-        result = greater(number_1, number_2)
-    elif operation == 'less':
-        result = less(number_1, number_2)
-    elif operation == 'equal':
-        result = equal(number_1, number_2)
-'''
